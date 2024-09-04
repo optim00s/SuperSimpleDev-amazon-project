b204eb69dc69cba2e22e400135ef94da0c73bafd
feyzsharaf <feyzullayevsharafx1@protonmail.com>
2024-09-04 16:32:17 +0400

    refactor quantity selection logic

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -27,7 +27,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -68,6 +68,10 @@ document.querySelectorAll('.js-add-to-cart')
 
             let matchingItem;
 
+            const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+
+            const quantity = Number(quantitySelector.value);  
+
             cart.forEach((item) => {
                 if (productId === item.productId) {
                     matchingItem = item;
@@ -75,11 +79,11 @@ document.querySelectorAll('.js-add-to-cart')
             });
 
             if (matchingItem) {
-                matchingItem.quantity += 1;
+                matchingItem.quantity += quantity;
             } else {
                 cart.push({
                     productId: productId,
-                    quantity: 1
+                    quantity: quantity
                 });    
             }
 
